fa39ed182242bd596a741a2ff8a21f01f5b287c0
Pascal Andy <pascal@firepress.org>
2021-01-11 23:29:54 -0500

    update default vars
    Signed-off-by: Pascal Andy <pascal@firepress.org>

--- var-config.sh
+++ var-config.sh
@@ -4,11 +4,15 @@ set -o errtrace         # Make sure any error trap is inherited
 set -o pipefail         # Use last non-zero exit code in a pipeline
 set -o nounset
 
-
 # ghost version, find latest image ==> https://hub.docker.com/r/devmtl/ghostfire/tags
-GHOST_IMG="devmtl/ghostfire:stable_3.4.0_13ba527"
-# shutdown the container after 3600 seconds
-WAIT_TIMER="3600" 
+GHOST_IMG="devmtl/ghostfire:stable_3.40.5_4f6eb6b_2021-01-09_03H26s32"
+
+# Sync external theme(s) (OPTIONAL)
+#DIR_FROM_EXTERNAL="/Volumes/960G/_pascalandy/11_FirePress/GitLab/firepress-ghost-themes-private-grp/ghost-themes/nurui_firepress-org-en/nurui-stg"
+#DIR_INTO_EXTERNAL="${DIR_MY_THEMES}/nurui-stg"
+
+DIR_FROM_EXTERNAL="/Volumes/960G/_pascalandy/11_FirePress/Github/github_pascalandy/Casper/dist/casper-search"
+DIR_INTO_EXTERNAL="${DIR_MY_THEMES}/casper-search"
 
 # --- DO NOT UPDATE ==> These var are relative to this projet
 export ROOT_PROJECT="$(pwd)"
@@ -18,7 +22,5 @@ DIR_CONTENT_THEMES="${ROOT_PROJECT}/content/themes"
 CTN_NAME="ghostUAT"
 CONTAINER_PATH="/var/lib/ghost/content"
 ALPINE_IMG="devmtl/alpine-base:3.8A"
+WAIT_TIMER="3600" # shutdown the container after 3600 seconds ( 1 hour)
 
-# Sync my external theme(s) (OPTIONAL)
-DIR_FROM_EXTERNAL="/Volumes/960G/_pascalandy/11_FirePress/GitLab/firepress-ghost-themes-private-grp/ghost-themes/nurui_firepress-org-en/nurui-stg"
-DIR_INTO_EXTERNAL="${DIR_MY_THEMES}/nurui-stg"
